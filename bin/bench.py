f8ed1f24e3a27d48f5e5c8e93e48295f3ec9e0ab
ZiluTian <zilu.tian@epfl.ch>
2023-03-31 16:46:33 +0200

    Bug fix

--- bin/bench.py
+++ bin/bench.py
@@ -14,14 +14,14 @@ def run(cores, cfreqs, cintervals):
             for cint in cintervals:
                 for experiment in EXPERIMENTS:
                     for input_file in config[experiment]['input_graph']:
-                        print(f"Running {experiment} cores {core} communication frequency is {cfreq} computation interval is {cint}")
+                        print(f"Running {input_file} {experiment} cores {core} communication frequency is {cfreq} computation interval is {cint}")
                         cp = config[experiment]["classpath"]
                         now = datetime.now()
                         current_time = now.strftime("%H%M%S")
                         if not os.path.exists(f"{LOG_DIR}/{ITERATION}"):
                             os.makedirs(f"{LOG_DIR}/{ITERATION}")
                         log_file = open(f"{LOG_DIR}/{ITERATION}/{MICRO_BENCHMARK}_{experiment}_cores{core}_cfreq{cfreq}_cint{cint}_{current_time}", 'a')
-                        process = subprocess.run([f'{FLINK_HOME}/bin/flink', 'run', '-c', cp, '-Dexecution.runtime-mode=BATCH', 'target/scala-2.12/benchmark-assembly-0.1-SNAPSHOT.jar', str(core), input_file, str(cfreq), str(cint)], text=True, stdout=subprocess.PIPE, check=True)
+                        process = subprocess.run([f'{SPARK_HOME}/bin/spark-submit', '--master', SPARK_MASTER, '--executor-cores', str(core), '--driver-memory', str(SPARK_DRIVER_MEM), '--executor-memory', str(SPARK_EXECUTOR_MEM), '--class', cp, 'target/scala-2.12/GraphxExperiments-assembly-1.0-SNAPSHOT.jar', input_file, str(cfreq), str(1)], text=True, stdout=subprocess.PIPE, check=True)
                         print(process.stdout, file=log_file)
                         os.system('echo 3 > /proc/sys/vm/drop_caches')
                         log_file.flush()
@@ -37,7 +37,7 @@ if (__name__ == "__main__"):
         elif arg == '-a':
             assemble = True
     
-    if ((MICRO_BENCHMARK == "") or (not os.path.exists(f"conf/{MICRO_BENCHMARK}.json"))):
+    if (MICRO_BENCHMARK == "" or (not os.path.exists(f"conf/{MICRO_BENCHMARK}.json"))):
         print("The conf file for the micro-benchmark is not found")
         exit(1)
 
@@ -48,8 +48,11 @@ if (__name__ == "__main__"):
     REPEAT = config['repeat']
     LOG_DIR = config['log_dir']
 
-    FLINK_HOME=config['flink']['home']
-   
+    SPARK_HOME=config['spark']['home']
+    SPARK_MASTER=config['spark']['master']
+    SPARK_DRIVER_MEM=config['spark']['driver_mem']
+    SPARK_EXECUTOR_MEM=config['spark']['executor_mem']
+
     subprocess.run(['mkdir', '-p', LOG_DIR], text=True, stdout=subprocess.PIPE, check=True)
     
     if (assemble):
