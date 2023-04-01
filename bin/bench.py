26f679cadba1bb2ccdb1544522b6ae800e06de3b
ZiluTian <zilu.tian@epfl.ch>
2023-04-01 19:36:44 +0200

    Update the driver script

--- bin/bench.py
+++ bin/bench.py
@@ -7,6 +7,7 @@ from datetime import datetime
 
 # user input
 MICRO_BENCHMARK = ""
+UBER_JAR = "target/scala-2.12/SparkExperiments-assembly-1.0-SNAPSHOT.jar"
 
 def run(cores, cfreqs, cintervals):
     for core in cores:
@@ -21,21 +22,17 @@ def run(cores, cfreqs, cintervals):
                         if not os.path.exists(f"{LOG_DIR}/{ITERATION}"):
                             os.makedirs(f"{LOG_DIR}/{ITERATION}")
                         log_file = open(f"{LOG_DIR}/{ITERATION}/{MICRO_BENCHMARK}_{experiment}_cores{core}_cfreq{cfreq}_cint{cint}_{current_time}", 'a')
-                        process = subprocess.run([f'{SPARK_HOME}/bin/spark-submit', '--master', SPARK_MASTER, '--executor-cores', str(core), '--driver-memory', str(SPARK_DRIVER_MEM), '--executor-memory', str(SPARK_EXECUTOR_MEM), '--class', cp, 'target/scala-2.12/GraphxExperiments-assembly-1.0-SNAPSHOT.jar', input_file, str(cfreq), str(1)], text=True, stdout=subprocess.PIPE, check=True)
+                        process = subprocess.run([f'{SPARK_HOME}/bin/spark-submit', '--master', SPARK_MASTER, '--executor-cores', str(core), '--driver-memory', str(SPARK_DRIVER_MEM), '--executor-memory', str(SPARK_EXECUTOR_MEM), '--class', cp, UBER_JAR, input_file, str(cfreq), str(cint)], text=True, stdout=subprocess.PIPE, check=True)
                         print(process.stdout, file=log_file)
                         os.system('echo 3 > /proc/sys/vm/drop_caches')
                         log_file.flush()
                         log_file.close()
 
 if (__name__ == "__main__"):
-    assemble = False
-
     for i in range(1, len(sys.argv)):
         arg = sys.argv[i]
         if arg == '-t':
             MICRO_BENCHMARK = sys.argv[i+1]
-        elif arg == '-a':
-            assemble = True
     
     if (MICRO_BENCHMARK == "" or (not os.path.exists(f"conf/{MICRO_BENCHMARK}.json"))):
         print("The conf file for the micro-benchmark is not found")
@@ -53,11 +50,13 @@ if (__name__ == "__main__"):
     SPARK_DRIVER_MEM=config['spark']['driver_mem']
     SPARK_EXECUTOR_MEM=config['spark']['executor_mem']
 
-    subprocess.run(['mkdir', '-p', LOG_DIR], text=True, stdout=subprocess.PIPE, check=True)
-    
-    if (assemble):
-        subprocess.run(['sbt', 'assembly'], text=True, stdout=subprocess.PIPE, check=True)
-        print(f"Assemble jar file for deployment completed")
+    if (not os.path.exists(LOG_DIR)):
+        os.makedirs(f"{LOG_DIR}")
+
+    # Always clean the build before running experiments, which removes the uber jar
+    subprocess.run(['sbt', 'clean'])
+    # Generate the uber jar after 
+    subprocess.run(['sbt', '-DsparkDeploy=Cluster', 'assembly'], text=True, stdout=subprocess.PIPE, check=True)
 
     for i in range(REPEAT):
         ITERATION = i
